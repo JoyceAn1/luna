a4c97bf67863335669c9ec56ec44a23a0da7547b
joyce anne <lunajs.197.stud@cdd.edu.ph>
2024-07-18 16:41:08 +0800

    laravel

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,12 @@
+<form action="">
+<div>
+    <p>Title:</p>
+    <input type="text" name="title" id="title">
+</div>
+
+<div>
+    <p>Content:</p><input types="text" name="content" is="content">
+</div>
+<br>
+<button>Save<button>
+</form>
